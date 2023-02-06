43d7b5cb6556394aabd835e977fa453f18d7c848
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-02-06 19:43:26 -0300

    Change order has many relationship to fix destroy order issue

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :products, through: :order_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
 
   validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :status, presence: true
