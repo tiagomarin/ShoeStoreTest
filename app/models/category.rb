561fee432d3bb4fa7848494e25309dcb2aa03483
Tiago Marin <tiago.lelinski@gmail.com>
2023-02-08 17:55:46 -0300

    Add brand and category tables and adjust views

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+  has_many :products
+
+  validates :name, presence: true, uniqueness: true,
+                   length: { maximum: 30, too_long: '%<count>s characters is the maximum allowed' }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_165527) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_204419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_165527) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -73,6 +85,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_165527) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "brand_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -93,4 +109,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_165527) do
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
 end
