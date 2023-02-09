04cb4f130f98c4241fa09202ec875d249da4c3b9
Tiago Marin <tiago.lelinski@gmail.com>
2023-02-09 18:15:03 -0300

    Add functionality to open and close modals that add a new brand or category
    Fix bugs in form in new product page

---
+++ app/javascript/components/popUp.js
@@ -0,0 +1,25 @@
+const brandModal = document.querySelector("#brand-modal")
+const categoryModal = document.querySelector("#category-modal")
+
+document.querySelectorAll(".open-modal").forEach( button => 
+  button.addEventListener( "click" , (e) => {
+    if (e.target.innerHTML === "New Brand") {
+      brandModal.classList.add("show")
+    }
+    if (e.target.innerHTML === "New Category") {
+      categoryModal.classList.add("show")
+    }
+  })
+)
+
+document.querySelectorAll(".close-modal").forEach( button => 
+  button.addEventListener( "click" , (e) => {
+    if (e.target.parentElement.id === "brand-modal") {
+      brandModal.classList.remove("show")
+    }
+    if (e.target.parentElement.id === "category-modal") {
+      categoryModal.classList.remove("show")
+    }
+  })
+)
+
