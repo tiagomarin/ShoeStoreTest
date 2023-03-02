04e8f793852fd0858becdcb74589cd5fedac9928
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-03-02 14:44:38 -0300

    Fix rubocop

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,4 @@
 class FavoritesController < ApplicationController
-
   def index
     @favorites = current_user.favorites
   end
@@ -8,15 +7,15 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.build(user_id: params[:user_id], product_id: params[:product_id])
     @favorite.save
     if @favorite.save
-      redirect_back fallback_location: root_path, notice: "Product favorited!"
+      redirect_back fallback_location: root_path, notice: 'Product favorited!'
     else
-      redirect_back fallback_location: root_path, alert: "Error favoriting product."
+      redirect_back fallback_location: root_path, alert: 'Error favoriting product.'
     end
   end
 
   def destroy
     @favorite = current_user.favorites.find(params[:id])
     @favorite.destroy
-    redirect_back fallback_location: root_path, notice: "Product unfavorited!"
+    redirect_back fallback_location: root_path, notice: 'Product unfavorited!'
   end
 end
