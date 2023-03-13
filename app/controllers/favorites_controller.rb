49ef15fc5860b487eb630c3735346064a65d0ee3
Tiago Marin <tiago.lelinski@gmail.com>
2023-03-13 15:47:57 -0300

    Add favorite button to show page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,16 +6,18 @@ class FavoritesController < ApplicationController
   def create
     @favorite = current_user.favorites.build(user_id: params[:user_id], product_id: params[:product_id])
     @favorite.save
-    if @favorite.save
-      redirect_back fallback_location: root_path, notice: 'Product favorited!'
-    else
-      redirect_back fallback_location: root_path, alert: 'Error favoriting product.'
-    end
+    # if @favorite.save
+    #   # redirect_back fallback_location: root_path, notice: 'Product favorited!'
+    #   alert: 'Product favorited!'
+    # else
+    #   # redirect_back fallback_location: root_path, alert: 'Error favoriting product.'
+    #   alert: 'Error favoriting product.'
+    # end
   end
 
   def destroy
-    @favorite = current_user.favorites.find(params[:id])
+    @favorite = current_user.favorites.find_by(product_id: params[:id])
     @favorite.destroy
-    redirect_back fallback_location: root_path, notice: 'Product unfavorited!'
+    # redirect_back fallback_location: root_path, notice: 'Product unfavorited!'
   end
 end
